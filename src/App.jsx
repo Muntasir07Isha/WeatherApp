57d1affe5af1675606bcb6888dcd64bbcca86e17
Muntasir <iammunta321@gmail.com>
2024-04-14 04:01:04 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import WeatherApp from './Components/WeatherApp/WeatherApp'
+import './App.css'
+
+function App() {
+  
+  return (
+    
+      <div className='App'>
+        <WeatherApp/>
+      </div>
+  )
+}
+
+export default App
